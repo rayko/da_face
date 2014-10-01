d0b703ce28c5846b2d1158d28789d622c19f4c20
Rayko <rayko.drg@gmail.com>
2014-10-01 11:48:02 -0300

    Adding more datasift sub-objects

--- lib/da_face.rb
+++ lib/da_face.rb
@@ -4,6 +4,11 @@ require "da_face/datasift/parser"
 require "da_face/datasift/errors"
 require "da_face/datasift/da_object"
 require "da_face/datasift/interaction"
+require "da_face/datasift/links"
+require "da_face/datasift/demographic"
+require "da_face/datasift/language"
+require "da_face/datasift/salience"
+require "da_face/datasift/twitter"
 require "json"
 
 module DaFace
--- spec/da_face/datasift/interaction_spec.rb
+++ spec/da_face/datasift/interaction_spec.rb
@@ -1,13 +1,38 @@
 require 'spec_helper'
 
 describe DaFace::Datasift::Interaction do
-  before do
-    @interaction_data = JSON.parse(File.read(fixture('interactions/simple.json')))['interaction']
+  describe '#new' do
+    before do
+      @interaction_data = JSON.parse(File.read(fixture('interactions/simple.json')))['interaction']
+    end
+    
+    it 'creates Interaction object with data' do
+      obj = DaFace::Datasift::Interaction.new @interaction_data
+      
+      expect(obj.class).to eq(DaFace::Datasift::Interaction)
+    end
   end
-  
-  it 'creates Interaction object with data' do
-    obj = DaFace::Datasift::Interaction.new @interaction_data
 
-    expect(obj.class).to eq(DaFace::Datasift::Interaction)
+
+  describe 'attributes' do
+    before do
+      @interaction_data = JSON.parse(File.read(fixture('interactions/simple.json')))['interaction']
+      @interaction = DaFace::Datasift::Interaction.new @interaction_data
+    end
+
+    it 'has all attributes required' do
+      expect(@interaction.author).to      eq(@interaction_data['author'])
+      expect(@interaction.content).to     eq(@interaction_data['content'])
+      expect(@interaction.created_at).to  eq(Time.parse(@interaction_data['created_at']))
+      expect(@interaction.id).to          eq(@interaction_data['id'])
+      expect(@interaction.link).to        eq(@interaction_data['link'])
+      expect(@interaction.received_at).to eq(Time.at(@interaction_data['received_at']))
+      expect(@interaction.schema).to      eq(@interaction_data['schema'])
+      expect(@interaction.source).to      eq(@interaction_data['source'])
+      expect(@interaction.type).to        eq(@interaction_data['type'])
+      expect(@interaction.tags).to        eq(@interaction_data['tags'])
+      expect(@interaction.tag_tree).to    eq(@interaction_data['tag_tree'])
+
+    end
   end
 end
