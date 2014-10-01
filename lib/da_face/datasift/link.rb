0c2da07ac573c46417515bcc3186f50b599fcf0f
Rayko <rayko.drg@gmail.com>
2014-10-01 14:26:01 -0300

    Added link specs and features

--- lib/da_face/datasift/link.rb
+++ lib/da_face/datasift/link.rb
@@ -1,8 +1,27 @@
 module DaFace
   module Datasift
     class Link
+      attr_accessor :code, :created_at, :hops, :meta, :normalized_url,
+                    :retweet_count, :title, :url
 
       def initialize data
+        self.code = data['code']
+        self.created_at = parsed_datetime(data['created_at'])
+        self.hops = data['hops'].collect{|d| string_to_uri(d)}
+        self.meta = data['meta']
+        self.normalized_url = string_to_uri(data['normalized_url'])
+        self.retweet_count = data['retweet_count']
+        self.title = data['title']
+        self.url = string_to_uri(data['url'])
+      end
+
+      private
+      def parsed_datetime datetime
+        Time.parse(datetime)
+      end
+
+      def string_to_uri url
+        URI(url)
       end
     end
   end
--- spec/da_face/datasift/link_spec.rb
+++ spec/da_face/datasift/link_spec.rb
@@ -13,4 +13,27 @@ describe DaFace::Datasift::Link do
       expect(obj.class).to eq(DaFace::Datasift::Link)
     end
   end
+
+  describe 'attributes' do
+    before do
+      @single_link = json_fixture('links/simple.json')
+      @single_link = DaFace::Datasift::Links.get_elements(@single_link).first
+      @link = DaFace::Datasift::Link.new @single_link
+    end
+
+    it 'has all required attributes' do
+      expect(@link.code).to eq(@single_link['code'])
+      expect(@link.created_at).to eq(Time.parse(@single_link['created_at']))
+      expect(@link.hops.class).to eq(Array)
+      @link.hops.each do |hop|
+        expect(hop.kind_of?(URI)).to eq(true)
+      end
+      expect(@link.meta).to eq(@single_link['meta'])
+      expect(@link.normalized_url).to eq(URI(@single_link['normalized_url']))
+      expect(@link.retweet_count).to eq(@single_link['retweet_count'])
+      expect(@link.title).to eq(@single_link['title'])
+      expect(@link.url.kind_of?(URI)).to eq(true)
+      expect(@link.url).to eq(URI(@single_link['url']))
+    end
+  end
 end
