1874455a353b19d60f2ce53e0865ecfc37732786
Rayko <rayko.drg@gmail.com>
2014-10-01 12:12:46 -0300

    Added language specs

--- lib/da_face/datasift/language.rb
+++ lib/da_face/datasift/language.rb
@@ -1,6 +1,14 @@
 module DaFace
   module Datasift
     class Language
+      attr_accessor :confidence, :tag, :tag_extended
+
+      def initialize data
+        self.confidence = data['confidence']
+        self.tag = data['tag']
+        self.tag_extended = data['tag_extended']
+      end
+      
     end
   end
 end
--- spec/da_face/datasift/language_spec.rb
+++ spec/da_face/datasift/language_spec.rb
@@ -13,4 +13,19 @@ describe DaFace::Datasift::Language do
     end
   end
 
+  describe 'attributes' do
+    before do
+      @data = JSON.parse(File.read(fixture('interactions/simple.json')))['language']
+      @language = DaFace::Datasift::Language.new @data
+    end
+    
+    it 'has all attributes required' do
+      expect(@language.confidence).to eq(@data['confidence'])
+      expect(@language.tag).to eq(@data['tag'])
+      expect(@language.tag_extended).to eq(@data['tag_extended'])
+    end
+  end
+
+  
+
 end
