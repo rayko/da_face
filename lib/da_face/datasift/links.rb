63b466e446e98dfd9bf943271fca599296f375ed
Rayko <rayko.drg@gmail.com>
2014-10-01 13:58:52 -0300

    Added link class for single links

--- lib/da_face/datasift/links.rb
+++ lib/da_face/datasift/links.rb
@@ -4,6 +4,32 @@ module DaFace
 
       def initialize data
       end
+      
+      def self.get_elements data
+        hashed_links = []
+        data.values.first.size.times do |index|
+          hashed_links << get_values(index - 1, data.keys, data)
+        end
+
+        return hashed_links
+      end
+
+      # Recursively search the hash for elements of index 'index'
+      # from the arrays inside the hashes.
+      def self.get_values index, keys, hash
+        new_hash = {}
+        keys.each do |key|
+          if hash[key].kind_of? Hash
+            new_hash[key] = get_values(index, hash[key].keys, hash[key])
+          else
+            new_hash[key] = hash[key][index]
+          end
+        end
+        
+        return new_hash
+      end
+
+
     end
   end
 end
--- spec/da_face/datasift/links_spec.rb
+++ spec/da_face/datasift/links_spec.rb
@@ -12,4 +12,52 @@ describe DaFace::Datasift::Links do
       expect(obj.class).to eq(DaFace::Datasift::Links)
     end
   end
+
+  describe '#get_values' do
+    before do
+      @hash = {
+        'key_1' => [1,2], 
+        'key_2' => {'key_3' => [3,4]},
+        'key_4' => {'key_5' => {'key_6' => [5,6]}}
+      }
+    end
+    it 'returns specific elements of arrays inside the hash' do
+      new_hash = DaFace::Datasift::Links.get_values(0, @hash.keys, @hash)
+
+      expect(new_hash['key_1'].class).not_to eq(Array)
+      expect(new_hash['key_1']).to eq(1)
+      expect(new_hash['key_2'].class).to eq(Hash)
+      expect(new_hash['key_2']['key_3']).to eq(3)
+      expect(new_hash['key_4']['key_5']['key_6']).to eq(5)
+    end
+  end
+
+  describe '#get_elements' do
+    context 'for single link' do
+      before do
+        @data = JSON.parse(File.read(fixture('links/simple.json')))
+      end
+
+      it 'gets first (0) elements of embebbed arrays' do
+        hashed_links = DaFace::Datasift::Links.get_elements @data
+
+        expect(hashed_links.class).to eq(Array)
+        expect(hashed_links.size).to eq(1)
+      end
+
+    end
+
+    context 'for multiples links' do
+      before do
+        @data = JSON.parse(File.read(fixture('links/multiples.json')))
+      end
+      
+      it 'gets first (0) elements of embebbed arrays' do
+        hashed_links = DaFace::Datasift::Links.get_elements @data
+
+        expect(hashed_links.class).to eq(Array)
+        expect(hashed_links.size).to eq(2)
+      end
+    end
+  end
 end
